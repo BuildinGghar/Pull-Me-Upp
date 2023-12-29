c33033b95ffee5357d027b46b3de2538a6cf2f8a
BuildinGghar <it@buildingghar.com>
2023-12-29 13:00:44 +0530

    1st

---
+++ webapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import *
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('about', about, name='about'),
+    path('services', services, name='services'),
+    path('contact', contact, name='contact'),
+    path('blog', blog, name='blog'),
+    path('blog_details', blog_details, name='blog_details'),
+    
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
